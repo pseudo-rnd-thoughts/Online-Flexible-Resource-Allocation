24efcdcfde1e769f637569cd3f49f68899a753d6
StringTheory <mark.m.towers@gmail.com>
2020-02-11 16:09:59 +0000

    Implementation of the environment settings with loading and saving

--- core/environment.py
+++ core/environment.py
@@ -6,6 +6,7 @@ from math import inf
 from random import choice
 from typing import List
 
+import core.log as log
 from core.server import Server
 from core.task import Task
 
@@ -17,27 +18,38 @@ class Environment:
         to maximise the social welfare of the system.
     """
 
-    def __init__(self, environment_setting: str, servers: List[Server], tasks: List[Task]):
-        self.environment_setting: str = environment_setting
+    debug: bool = True
+
+    def __init__(self, name: str, servers: List[Server], tasks: List[Task], total_time_steps: int):
+        self.name: str = name
+
         self.servers: List[Server] = servers
-        self.unallocated_tasks: List[Task] = tasks
-        self.time: int = 0
 
-    def time_step(self):
-        """
-        Simulates a time step of the environment with both stages (sub-environments) of the problem
-        """
+        self.tasks: List[Task] = tasks
+        self.unallocated_tasks: List[Task] = sorted(tasks, key=lambda task: task.auction_time)
+
+        self.time_step: int = 0
+        self.total_time_steps: int = total_time_steps
+
+    def tick(self):
+        """Simulates a time step of the environment with both stages (sub-environments) of the problem"""
+        log.info('Environment - Time Step: {}'.format(self.time_step))
+
         # Stage 1: auction the unallocated tasks
-        auction_tasks = [task for task in self.unallocated_tasks if self.time <= task.start_time]
+        auction_tasks = [task for task in self.unallocated_tasks if self.time_step <= task.auction_time]
 
+        log.info('Auction tasks')
         for task in auction_tasks:
+            log.info('\t{}'.format(task))
+
             # Implementation of a Vickrey auction
             min_price: float = inf
             second_min_price: float = inf - 1
             min_server: List[Server] = []
 
             for server in self.servers:
-                price = server.price_task(task)
+                price = server.price_task(task, self.time_step)
+                log.debug('\t\tServer {} price: {}'.format(server.name, price))
 
                 if price == -1:  # If a server doesnt wish to bid on a task then just returns -1
                     continue
@@ -52,12 +64,17 @@ class Environment:
             if len(min_server):
                 server = choice(min_server)
                 server.allocate_task(task, second_min_price)
-                task.allocate_server(server, second_min_price)
-
+                task.allocate_server(server, second_min_price, self.time_step)
                 self.unallocated_tasks.remove(task)
 
-        # Stage 2: allocate the resources for each server
-        for server in self.servers:
-            server.allocate_resources()
+                log.info('\tMin Price: {}, Winning Server: {}, Second min price: {}'
+                         .format(min_price, server.name, second_min_price))
+            else:
+                log.info('\tNo minimum price')
 
-        self.time += 1
+        # Stage 2: allocate the resources for each server
+        log.info('\nAllocate the resources')
+        for server in self.servers:
+            server.allocate_resources(self.time_step)
+
+        self.time_step += 1
---
+++ settings/setting.py
@@ -0,0 +1,145 @@
+"""Setting module for loading and saving environment and loading environment settings"""
+
+import json
+from random import randint, choice
+from typing import List
+
+from core.environment import Environment
+from core.server import Server
+from core.task import Task
+
+
+def load_environment(filename: str) -> Environment:
+    """
+    Loads an environment from a file
+    :param filename: The filename to load the environment from
+    :return: The loaded environment
+
+                Template
+    {"name": "",
+     "total time steps": 0,
+     "servers": [{"name": "", "storage capacity": 0, "computational capacity": 0, "bandwidth capacity": 0}, ...],
+     "tasks": [{"name": "", "required storage": 0, "required computation": 0, "required results data": 0,
+                "auction time": 0, "deadline": 0}, ...]}
+    """
+
+    with open(filename) as file:
+        json_data = json.load(file)
+
+        env_name: str = json_data['name']
+        total_time_steps: int = json_data['total time steps']
+        servers: List[Server] = [
+            Server(server_data['name'], server_data['storage capacity'], server_data['computational capacity'],
+                   server_data['bandwidth capacity'])
+            for server_data in json_data['servers']
+        ]
+        tasks: List[Task] = [
+            Task(task_data['name'], task_data['auction time'], task_data['deadline'],
+                 task_data['required storage'], task_data['required computational'], task_data['required results data'])
+            for task_data in json_data['tasks']
+        ]
+
+    return Environment(env_name, servers, tasks, total_time_steps)
+
+
+def save_environment(environment: Environment, filename: str):
+    """
+    Saves an environment to a file
+    :param environment: The environment to save
+    :param filename: The filename to save the environment to
+
+                Template
+    {"name": "",
+     "total time steps": 0,
+     "servers": [{"name": "", "storage capacity": 0, "computational capacity": 0, "bandwidth capacity": 0}, ...],
+     "tasks": [{"name": "", "required storage": 0, "required computation": 0, "required results data": 0,
+                "auction time": 0, "deadline": 0}, ...]}
+    """
+
+    environment_json_data = {
+        'name': environment.name,
+        'total time steps': environment.total_time_steps,
+        'servers': [
+            {'name': server.name, 'storage capacity': server.storage_capacity,
+             'computational capacity': server.computational_capacity, 'bandwidth capacity': server.bandwidth_capacity}
+            for server in environment.servers
+        ],
+        'tasks': [
+            {'name': task.name, 'required storage': task.required_storage,
+             'required computational': task.required_computation, 'required results data': task.required_results_data,
+             'auction time': task.auction_time, 'deadline': task.deadline}
+            for task in environment.tasks
+        ]
+    }
+
+    with open(filename, 'w') as file:
+        json.dump(environment_json_data, file)
+
+
+def load_environment_settings(filename: str, num_envs: int) -> List[Environment]:
+    """
+    Load an environment settings from a file with a number of environments
+    :param filename: The filename to loads the settings from
+    :param num_envs: The number of environment to create
+    :return: A list of environments created using the environments
+
+        Template
+    {
+      "name": "",
+      "min total time steps": 0, "max total time steps": 0,
+      "min total servers": 0, "max total servers": 0,
+      "server settings": [
+        {
+          "name": "", "min storage capacity": 0, "max storage capacity": 0,
+          "min computational capacity": 0, "max computational capacity": 0,
+          "min bandwidth capacity": 0, "max bandwidth capacity": 0
+        }, ...
+      ],
+      "task settings": [
+        {
+          "name": "", "min deadline": 0, "max deadline": 0,
+          "min required storage": 0, "max required storage": 0,
+          "min required computation": 0, "max required computation": 0,
+          "min required results data": 0, "max required results data": 0
+        }, ...
+      ]
+    }
+    """
+
+    environments: List[Environment] = []
+    with open(filename) as file:
+        env_setting_json = json.load(file)
+
+    for env_num in range(num_envs):
+        env_name = '{} {}'.format(env_setting_json['name'], env_num)
+        total_time_steps = randint(env_setting_json['min total time steps'],
+                                   env_setting_json['max total time steps'])
+        servers: List[Server] = []
+        for server_num in range(randint(env_setting_json['min total servers'],
+                                        env_setting_json['max total servers'])):
+            server_json_data = choice(env_setting_json['server settings'])
+            servers.append(Server('{} {}'.format(server_json_data['name'], server_num),
+                                  randint(server_json_data['min storage capacity'],
+                                          server_json_data['max storage capacity']),
+                                  randint(server_json_data['min computational capacity'],
+                                          server_json_data['max computational capacity']),
+                                  randint(server_json_data['min bandwidth capacity'],
+                                          server_json_data['max bandwidth capacity'])))
+
+        tasks: List[Task] = []
+        for task_num in range(randint(env_setting_json['min total tasks'],
+                                      env_setting_json['max total tasks'])):
+            task_json_data = choice(env_setting_json['task settings'])
+            auction_time = randint(0, total_time_steps)
+            tasks.append(Task('{} {}'.format(task_json_data['name'], task_num), auction_time,
+                              auction_time + randint(task_json_data['min deadline'], task_json_data['max deadline']),
+                              randint(task_json_data['min required storage'],
+                                      task_json_data['max required storage']),
+                              randint(task_json_data['min required computation'],
+                                      task_json_data['max required computation']),
+                              randint(task_json_data['min required results data'],
+                                      task_json_data['max required results data'])))
+
+        environments.append(Environment(env_name, servers, tasks, total_time_steps))
+
+    return environments
