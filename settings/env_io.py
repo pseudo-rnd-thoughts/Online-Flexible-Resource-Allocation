8754d24782f6d337eedddc4dec0afba39a8e4587
StringTheory <mark.m.towers@gmail.com>
2020-02-24 15:27:34 +0000

    Updated the environment

--- settings/env_io.py
+++ settings/env_io.py
@@ -13,44 +13,6 @@ if TYPE_CHECKING:
     from env.environment import OnlineFlexibleResourceAllocationEnv
 
 
-def load_environment(filename: str) -> Tuple[str, List[Server], List[Task], int]:
-    """
-    Loads an environment from a file
-    :param filename: The filename to load the environment from
-    :return: The loaded environment
-
-                Template
-    {"name": "",
-     "total time steps": 0,
-     "servers": [{"name": "", "storage capacity": 0, "computational capacity": 0, "bandwidth capacity": 0}, ...],
-     "tasks": [{"name": "", "required storage": 0, "required computation": 0, "required results data": 0,
-                "auction time": 0, "deadline": 0}, ...]}
-    """
-
-    with open(filename) as file:
-        json_data = json.load(file)
-
-        name: str = json_data['env name']
-        total_time_steps: int = json_data['total time steps']
-
-        # Load the servers list
-        servers: List[Server] = [
-            Server(name=server_data['name'], storage_cap=server_data['storage capacity'],
-                   comp_cap=server_data['computational capacity'], bandwidth_cap=server_data['bandwidth capacity'])
-            for server_data in json_data['servers']
-        ]
-
-        # Load the tasks list
-        tasks: List[Task] = [
-            Task(name=task_data['name'], auction_time=task_data['auction time'], deadline=task_data['deadline'],
-                 required_storage=task_data['required storage'], required_comp=task_data['required computational'],
-                 required_results_data=task_data['required results data'])
-            for task_data in json_data['tasks']
-        ]
-
-    return name, servers, tasks, total_time_steps
-
-
 def save_environment(environment: OnlineFlexibleResourceAllocationEnv, filename: str):
     """
     Saves an environment to a file
