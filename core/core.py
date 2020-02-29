ab28e68700a955643d6ec1dfbf0dcf111761ada5
StringTheory <mark.m.towers@gmail.com>
2020-02-29 15:09:00 +0000

    Fixed the rounding function, finally

--- core/core.py
+++ core/core.py
@@ -6,3 +6,7 @@ class ResourceAllocationError(Exception):
 def assert_resource_allocation(cond, message=''):
     if not cond:
         raise ResourceAllocationError(message)
+
+
+def round_float(value: float) -> float:
+    return round(value, 4)
