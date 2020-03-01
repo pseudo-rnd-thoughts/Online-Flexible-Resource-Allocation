34d0ac59bf3d3daf9041213c6b44de87c390284d
StringTheory <mark.m.towers@gmail.com>
2020-03-01 22:27:26 +0000

    Added environment step random function test

--- tests/test_envionment_step.py
+++ tests/test_envionment_step.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from env.environment import OnlineFlexibleResourceAllocationEnv
 
 
-def test_environment_rnd():
+def test_env_step_action_rnd():
     env = OnlineFlexibleResourceAllocationEnv.make('../settings/basic_env.json')
 
     for _ in tqdm(range(1000)):
@@ -28,3 +28,13 @@ def test_environment_rnd():
                 }
 
             state, reward, done, info = env.step(actions)
+
+
+def test_env_auction_step():
+    # Todo
+    pass
+
+
+def test_env_resource_allocation_step():
+    # Todo
+    pass
