1bfd98aac4f2c8388863c9a0759ed6a211e712cf
StringTheory <mark.m.towers@gmail.com>
2020-03-05 01:08:29 +0000

    Fixed a bug with next_auction_task using the time step during auction step and that server disappear after resource allocation step

--- tests/test_envionment_step.py
+++ tests/test_envionment_step.py
@@ -7,17 +7,30 @@ from env.environment import OnlineFlexibleResourceAllocationEnv
 
 
 def test_env_step_action_rnd():
-    env = OnlineFlexibleResourceAllocationEnv.make('../settings/basic_env.json')
+    print()
+    env = OnlineFlexibleResourceAllocationEnv.make('../src/settings/basic_env.json')
 
-    for _ in tqdm(range(1000)):
+    for _ in tqdm(range(20)):
         state = env.reset()
-        done = False
 
+        num_tasks = len(env.unallocated_tasks) + (1 if state.auction_task else 0)
+        # print(f'Auction tasks ({num_tasks}) at [' + ', '.join(str(task.auction_time) for task in env.unallocated_tasks) + ']')
+        auctioned_tasks = 0
+
+        num_servers = len(state.server_tasks)
+        # print(f'Num of servers: {num_servers}')
+
+        done = False
         while not done:
+            # print(f'\tUnallocated tasks: {len(env.unallocated_tasks)}')
+            # print(f'State num of servers: {len(state.server_tasks)}')
+            assert len(state.server_tasks) == num_servers
             if state.auction_task:
                 actions = {
                     server: rnd.randint(1, 20) for server in state.server_tasks.keys()
                 }
+                # print(f'\tAuction of {state.auction_task.name}, time step: {state.time_step}')
+                auctioned_tasks += 1
             else:
                 actions = {
                     server: {
@@ -26,8 +39,12 @@ def test_env_step_action_rnd():
                     }
                     for server, tasks in state.server_tasks.items()
                 }
-
+                # print(f'\tResource allocation')
+            # print(f'Step, time step: {env.state.time_step}')
             state, reward, done, info = env.step(actions)
+
+        # print(f'Num unallocated tasks: {num_tasks}, auctioned tasks: {auctioned_tasks}\n')
+        assert num_tasks == auctioned_tasks
 
 
 def test_env_auction_step():
