faa25c1a65ec86c7eeb50b92c5abd36b03546249
StringTheory <mark.m.towers@gmail.com>
2020-03-09 16:17:36 +0000

    Fixed the testing functions due to changes in Agents V2

--- tests/network_differential.py
+++ tests/network_differential.py
@@ -1,4 +1,7 @@
-from agents.task_pricing_agent import TaskPricingAgent
+"""Checks that neural networks can be differentiated using DQN loss function"""
+
+from agents.rl_agents.neural_networks.dqn_network import dqn_lstm_network
+from agents.rl_agents.rl_agent import TaskPricingRLAgent
 from env.server import Server
 from env.task import Task
 from env.task_stage import TaskStage
@@ -7,32 +10,33 @@ import tensorflow as tf
 
 
 def network_differential():
-    network = TaskPricingNetwork(num_outputs=10)
+    network = dqn_lstm_network(9, 10)
 
     server = Server('Test', 220.0, 35.0, 22.0)
     auction_task = Task('Test 4', 69.0, 35.0, 10.0, 0, 12)
     allocated_tasks = [
         Task('Test 1', 76.0, 36.0, 16.0, 0, 12, stage=TaskStage.COMPUTING, loading_progress=76.0),
-        Task('Test 2', 75.0, 37.0, 12.0, 0, 12, stage=TaskStage.COMPUTING, loading_progress=75.0, compute_progress=10.0),
+        Task('Test 2', 75.0, 37.0, 12.0, 0, 12, stage=TaskStage.COMPUTING, loading_progress=75.0,
+             compute_progress=10.0),
         Task('Test 3', 72.0, 47.0, 20.0, 0, 7, stage=TaskStage.COMPUTING, loading_progress=72.0, compute_progress=25.0)
     ]
     optimiser = tf.keras.optimizers.RMSprop(lr=0.001)
 
-    observation = TaskPricingAgent.network_observation(auction_task, allocated_tasks, server, 0)
+    observation = TaskPricingRLAgent.network_observation(auction_task, allocated_tasks, server, 0)
     network(observation)
 
     network_variables = network.trainable_variables
     with tf.GradientTape() as tape:
-        observation = TaskPricingAgent.network_observation(auction_task, allocated_tasks, server, 0)
+        observation = TaskPricingRLAgent.network_observation(auction_task, allocated_tasks, server, 0)
 
         action_q_values = network(observation)
         print(f'Action Q Values: {action_q_values}')
 
-        error = tf.reduce_mean(0.5 * tf.square(action_q_values))
+        loss = tf.reduce_mean(0.5 * tf.square(action_q_values))
 
-    network_gradients = tape.gradient(error, network_variables)
+    network_gradients = tape.gradient(loss, network_variables)
     optimiser.apply_gradients(zip(network_gradients, network_variables))
-    print(f'Error: {error}')
+    print(f'Loss: {loss}')
     print(f'Network Gradients: {network_gradients}')
 
 
--- tests/test_envionment_step.py
+++ tests/test_envionment_step.py
@@ -1,19 +1,24 @@
 """Testing the environment step function"""
 
-import random as rnd
 from tqdm import tqdm
 
+from agents.human_agents.human_agent import HumanTaskPricing, HumanResourceWeighting
+from agents.human_agents.random_agent import RandomTaskPricingAgent, RandomResourceWeightingAgent
 from env.environment import OnlineFlexibleResourceAllocationEnv
+from env.server import Server
+from env.task import Task
 
 
 def test_env_step_rnd_action():
     print()
     env = OnlineFlexibleResourceAllocationEnv.make('../src/env_settings/basic_env.json')
+    random_task_pricing = RandomTaskPricingAgent(0)
+    random_resource_weighting = RandomResourceWeightingAgent(0)
 
     for _ in tqdm(range(20)):
         state = env.reset()
 
-        num_tasks = len(env.unallocated_tasks) + (1 if state.auction_task else 0)
+        num_tasks = len(env._unallocated_tasks) + (1 if state.auction_task else 0)
         # print(f'Auction tasks ({num_tasks}) at [' + ', '.join(str(task.auction_time) for task in env.unallocated_tasks) + ']')
         auctioned_tasks = 0
 
@@ -27,14 +32,15 @@ def test_env_step_rnd_action():
             assert len(state.server_tasks) == num_servers
             if state.auction_task:
                 actions = {
-                    server: rnd.randint(1, 20) for server in state.server_tasks.keys()
+                    server: random_task_pricing.bid(state.auction_task, allocated_tasks, server, state.time_step)
+                    for server, allocated_tasks in state.server_tasks.items()
                 }
                 # print(f'\tAuction of {state.auction_task.name}, time step: {state.time_step}')
                 auctioned_tasks += 1
             else:
                 actions = {
                     server: {
-                        task: rnd.randint(1, 20)
+                        task: random_resource_weighting.weight(task, tasks, server, state.time_step)
                         for task in tasks
                     }
                     for server, tasks in state.server_tasks.items()
@@ -48,10 +54,57 @@ def test_env_step_rnd_action():
 
 
 def test_env_auction_step():
-    # Todo
-    pass
+    print()
+    human_task_pricing = HumanTaskPricing(0)
+
+    servers_tasks = {
+        Server(): []
+    }
+    tasks = [
+        Task()
+    ]
+    env, state = OnlineFlexibleResourceAllocationEnv.custom_env('auction step test', 3, servers_tasks, tasks)
+    print('State')
+    print(state)
+
+    actions = {
+        server: human_task_pricing.bid(state.auction_task, tasks, server, state.time_step)
+        for server, tasks in state.server_tasks.items()
+    }
+
+    next_state, rewards, done, info = env.step(actions)
+    print('Next state')
+    print(next_state)
+
+    print('Rewards - [' + ', '.join(f'{task.name} Task: {price}' for task, price in rewards.items()) + ']')
 
 
 def test_env_resource_allocation_step():
-    # Todo
-    pass
+    print()
+    human_resource_weighting = HumanResourceWeighting(0)
+
+    servers_tasks = {
+        Server(): [
+            Task(),
+            Task(),
+            Task()
+        ]
+    }
+    env, state = OnlineFlexibleResourceAllocationEnv.custom_env('resource weighting step test', 5, servers_tasks, [])
+    print('State')
+    print(state)
+
+    actions = {
+        server: {
+            task: human_resource_weighting.weight(task, tasks, server, state.time_step)
+            for task in tasks
+        }
+        for server, tasks in state.server_tasks.items()
+    }
+
+    next_state, rewards, done, info = env.step(actions)
+    print('Next state')
+    print(next_state)
+
+    print('rewards - {' + ', '.join(f'{server.name} server: [' + ', '.join(f'{task.name} Task: {task.stage}' for task in tasks) + ']'
+                                    for server, tasks in rewards.items()) + '}')
