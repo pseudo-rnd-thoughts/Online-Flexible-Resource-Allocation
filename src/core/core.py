9ca6fdf9af185764ea6593de72fe257fd72f9796
StringTheory <mark.m.towers@gmail.com>
2020-03-09 11:33:13 +0000

    Removed due to the code not being required anymore

--- src/core/core.py
+++
@@ -1,8 +0,0 @@
-
-def round_float(value: float) -> float:
-    """
-    Rounds a number to four decimal places, this is important for the task and server classes in resource allocation
-    :param value: The number that is normally a float
-    :return: Rounded number to four decimal places
-    """
-    return round(value, 4)
