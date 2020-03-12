416b5c2ee24aa95bdf547342962bd4e73a4ea593
StringTheory <mark.m.towers@gmail.com>
2020-03-12 11:12:34 +0000

    Created training agent environments

--- src/train_agents/training/multi_env_agent_train.py
+++ src/train_agents/training/multi_env_agent_train.py
@@ -3,26 +3,30 @@
 from agents.rl_agents.dqn import TaskPricingDqnAgent, ResourceWeightingDqnAgent
 from agents.rl_agents.neural_networks.dqn_networks import DqnBidirectionalLstmNetwork
 from env.environment import OnlineFlexibleResourceAllocationEnv
-from train.core import generate_eval_envs, run_training, setup_tensorboard
+from train_agents.core import generate_eval_envs, run_training, setup_tensorboard
 
 if __name__ == "__main__":
-    setup_tensorboard('basic_training_logs')
+    setup_tensorboard('multi_agent_logs')
 
-    env = OnlineFlexibleResourceAllocationEnv.make('../env_settings/basic_env.json')
-    eval_envs = generate_eval_envs(env, 5, 'basic_training_eval_envs')
+    env = OnlineFlexibleResourceAllocationEnv.make([
+        '../env_settings/basic_env.json'
+    ])
+    eval_envs = generate_eval_envs(env, 10, 'multi_agent_eval_envs')
 
     task_pricing_dqn_agents = [
-        TaskPricingDqnAgent(0, DqnBidirectionalLstmNetwork(0, 9, 10),
+        TaskPricingDqnAgent(agent_num, DqnBidirectionalLstmNetwork(agent_num, 9, 10),
                             replay_buffer_length=20000, training_replay_start_size=10000,
                             target_update_frequency=1000)
+        for agent_num in range(3)
     ]
     resource_weighting_dqn_agents = [
-        ResourceWeightingDqnAgent(0, DqnBidirectionalLstmNetwork(0, 10, 10),
+        ResourceWeightingDqnAgent(agent_num, DqnBidirectionalLstmNetwork(agent_num, 10, 10),
                                   replay_buffer_length=50000, training_replay_start_size=25000,
                                   target_update_frequency=10000)
+        for agent_num in range(3)
     ]
 
     run_training(env, eval_envs, 5000, task_pricing_dqn_agents, resource_weighting_dqn_agents, 5)
 
-    print(f'TP Total Obs: {task_pricing_dqn_agents[0].name}: {task_pricing_dqn_agents[0].total_obs}')
-    print(f'RW Total Obs: {resource_weighting_dqn_agents[0].name}: {resource_weighting_dqn_agents[0].total_obs}')
+    print('TP Total Obs: {' + ', '.join(f'{agent.name}: {agent.total_obs}' for agent in task_pricing_dqn_agents) + '}')
+    print('RW Total Obs: {' + ', '.join(f'{agent.name}: {agent.total_obs}' for agent in resource_weighting_dqn_agents) + '}')
