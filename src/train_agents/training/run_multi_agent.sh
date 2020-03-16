627f685089959c2e47e73e760e39af5d5baed11d
StringTheory <mark.m.towers@gmail.com>
2020-03-16 15:57:52 +0000

    Fixed shebang bug

--- src/train_agents/training/run_multi_agent.sh
+++ src/train_agents/training/run_multi_agent.sh
@@ -1,4 +1,4 @@
-#!/bin bash
+#!/bin/bash
 
 #SBATCH --time=04:00:00
 
@@ -7,7 +7,7 @@ cd ~/Online-Flexible-Resource-Allocation/src/
 module load conda
 source activate py37env
 
-echo 'Running multi-agent train'
+echo 'Running multi-agent DQN'
 python -m  train_agents.training.multi_agent_train
 
 # sbatch -p lyceum run_multi_agent.sh
