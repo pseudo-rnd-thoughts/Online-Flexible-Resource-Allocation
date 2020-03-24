9099765d1b9901456eca0655c1d1b800bf2034cc
StringTheory <mark.m.towers@gmail.com>
2020-03-24 23:30:29 +0000

    Added test for the standard_config.gin file will load completely

--- tests/test_gin_config.py
+++ tests/test_gin_config.py
@@ -7,12 +7,17 @@ import gin
 from agents.rl_agents.dqn import TaskPricingDqnAgent
 from agents.rl_agents.neural_networks.dqn_networks import DqnLstmNetwork
 
-gin.parse_config("""
-import agents.rl_agents.rl_agent
-
-ReinforcementLearningAgent.save_frequency = 4
-""")
-
 def test_agent_gin():
+    gin.parse_config("""
+    import agents.rl_agents.rl_agent
+
+    ReinforcementLearningAgent.save_frequency = 4
+    """)
+
     test_agent = TaskPricingDqnAgent(1, DqnLstmNetwork(1, 9, 10))
     assert test_agent.save_frequency == 4
+
+def test_standard_gin_config():
+    gin.parse_config_file('../src/train_agents/training/standard_config.gin')
+
+    test_agent = TaskPricingDqnAgent(1, DqnLstmNetwork(1, 9, 10))
