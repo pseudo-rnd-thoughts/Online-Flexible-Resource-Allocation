278778aa93ac47a06d69f40107e2d890614c17f1
StringTheory <mark.m.towers@gmail.com>
2020-03-29 22:38:54 +0100

    Update test exploration with assert for final exploration value

--- tests/test_exploration.py
+++ tests/test_exploration.py
@@ -3,10 +3,12 @@ def test_linear_exploration():
     initial_exploration = 1
     final_exploration = 0.1
     final_exploration_frame = 100000
+    update_exploration = 0
 
-    for obs in range(0, final_exploration_frame, 1000):
+    for obs in range(0, final_exploration_frame+1, 1000):
         update_exploration = obs * (final_exploration - initial_exploration) / final_exploration_frame + initial_exploration
-        print(update_exploration)
+        print(f'Obs: {obs}, Exploration: {update_exploration}')
 
+    assert round(update_exploration, 5) == final_exploration
 
 test_linear_exploration()
