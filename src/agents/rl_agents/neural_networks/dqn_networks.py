3eb8fc6bf6db4956aad909d9414066beb412b2d4
StringTheory <mark.m.towers@gmail.com>
2020-04-13 22:08:29 +0100

    Updated networks with new RNN network

--- src/agents/rl_agents/neural_networks/dqn_networks.py
+++ src/agents/rl_agents/neural_networks/dqn_networks.py
@@ -1,13 +1,13 @@
-"""Standard DQN Networks"""
+"""
+Dqn networks using variations on the recurrent network
+"""
 
 from __future__ import annotations
 
 import gin.tf
 import tensorflow as tf
 
-from agents.rl_agents.neural_networks.network import Network
-
-# Todo add regularizers to the networks
+# Todo add regularises to the networks
 
 
 @gin.configurable
@@ -32,9 +32,9 @@ def create_lstm_dqn_network(input_width: int, num_actions: int, lstm_width: int 
 
 
 @gin.configurable
-def create_gru_dqn_network(input_width: int, num_actions: int, lstm_width: int = 32, relu_width: int = 32):
+def create_gru_dqn_network(input_width: int, num_actions: int, gru_width: int = 32, relu_width: int = 32):
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
-    gru_layer = tf.keras.layers.GRU(lstm_width)(input_layer)
+    gru_layer = tf.keras.layers.GRU(gru_width)(input_layer)
     relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(gru_layer)
     q_layer = tf.keras.layers.Dense(num_actions, activation='linear')(relu_layer)
 
@@ -42,13 +42,29 @@ def create_gru_dqn_network(input_width: int, num_actions: int, lstm_width: int =
 
 
 @gin.configurable
-def create_lstm_dueling_dqn_network(input_width: int, num_actions: int, lstm_width: int = 32, relu_width: int = 32):
+def create_rnn_dqn_network(input_width: int, num_actions: int, rnn_width: int = 32, relu_width: int = 32):
+    input_layer = tf.keras.layers.Input(shape=(None, input_width))
+    rnn_layer = tf.keras.layers.RNN(rnn_width)(input_layer)
+    relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(rnn_layer)
+    q_layer = tf.keras.layers.Dense(num_actions, activation='linear')(relu_layer)
+
+    return tf.keras.Model(name='RNN Dqn', inputs=input_layer, outputs=q_layer)
+
+
+@gin.configurable
+def create_lstm_dueling_dqn_network(input_width: int, num_actions: int,
+                                    lstm_width: int = 32, relu_width: int = 32, combiner: str = 'avg'):
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
     lstm_layer = tf.keras.layers.LSTM(lstm_width)(input_layer)
     relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(lstm_layer)
     value = tf.keras.layers.Dense(1, activation='linear')(relu_layer)
     advantage = tf.keras.layers.Dense(num_actions, activation='linear')(relu_layer)
-    dueling_q_layer = value + (advantage - tf.reduce_mean(advantage, axis=1, keepdims=True))
+    if combiner == 'avg':
+        dueling_q_layer = value + (advantage - tf.reduce_mean(advantage, axis=1, keepdims=True))
+    elif combiner == 'mean':
+        dueling_q_layer = value + (advantage - tf.reduce_max(advantage, axis=1, keepdims=True))
+    else:
+        raise Exception(f'Unknown combiner function ({combiner})')
 
     return tf.keras.Model(name='LSTM Dueling Dqn', inputs=input_layer, outputs=dueling_q_layer)
 
