3b214edfec809c0c31d3392f429c3bec4ff9fa39
StringTheory <mark.m.towers@gmail.com>
2020-04-17 12:11:26 +0100

    Updated dqn networks to use the functional keras model api

--- src/agents/rl_agents/neural_networks/dqn_networks.py
+++ src/agents/rl_agents/neural_networks/dqn_networks.py
@@ -12,6 +12,18 @@ import tensorflow as tf
 
 @gin.configurable
 def create_bidirectional_dqn_network(input_width: int, num_actions: int, lstm_width: int = 32, relu_width: int = 32):
+    """
+    Creates a bidirectional lstm dqn network
+
+    Args:
+        input_width: Network input width
+        num_actions: Number of actions
+        lstm_width: Number of LSTM layer units
+        relu_width: Number of RELU layer units
+
+    Returns: tensorflow keras Model
+
+    """
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
     lstm_layer = tf.keras.layers.LSTM(lstm_width)
     bidirectional_layer = tf.keras.layers.Bidirectional(lstm_layer)(input_layer)
@@ -23,6 +35,18 @@ def create_bidirectional_dqn_network(input_width: int, num_actions: int, lstm_wi
 
 @gin.configurable
 def create_lstm_dqn_network(input_width: int, num_actions: int, lstm_width: int = 32, relu_width: int = 32):
+    """
+    Creates a lstm dqn network
+
+    Args:
+        input_width: Network input width
+        num_actions: Number of actions
+        lstm_width: Number of LSTM layer units
+        relu_width: Number of RELU layer units
+
+    Returns: tensorflow keras Model
+
+    """
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
     lstm_layer = tf.keras.layers.LSTM(lstm_width)(input_layer)
     relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(lstm_layer)
@@ -33,6 +57,18 @@ def create_lstm_dqn_network(input_width: int, num_actions: int, lstm_width: int 
 
 @gin.configurable
 def create_gru_dqn_network(input_width: int, num_actions: int, gru_width: int = 32, relu_width: int = 32):
+    """
+    Creates a gru dqn network
+
+    Args:
+        input_width: Network input width
+        num_actions: Number of actions
+        gru_width: Number of GRU layer units
+        relu_width: Number of RELU layer units
+
+    Returns: tensorflow keras Model
+
+    """
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
     gru_layer = tf.keras.layers.GRU(gru_width)(input_layer)
     relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(gru_layer)
@@ -43,8 +79,20 @@ def create_gru_dqn_network(input_width: int, num_actions: int, gru_width: int = 
 
 @gin.configurable
 def create_rnn_dqn_network(input_width: int, num_actions: int, rnn_width: int = 32, relu_width: int = 32):
+    """
+    Creates a rnn dqn network
+
+    Args:
+        input_width: Network input width
+        num_actions: Number of actions
+        rnn_width: Number of RNN layer units
+        relu_width: Number of RELU layer units
+
+    Returns: tensorflow keras Model
+
+    """
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
-    rnn_layer = tf.keras.layers.RNN(rnn_width)(input_layer)
+    rnn_layer = tf.keras.layers.SimpleRNN(rnn_width)(input_layer)
     relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(rnn_layer)
     q_layer = tf.keras.layers.Dense(num_actions, activation='linear')(relu_layer)
 
@@ -54,6 +102,19 @@ def create_rnn_dqn_network(input_width: int, num_actions: int, rnn_width: int = 
 @gin.configurable
 def create_lstm_dueling_dqn_network(input_width: int, num_actions: int,
                                     lstm_width: int = 32, relu_width: int = 32, combiner: str = 'avg'):
+    """
+    Creates a lstm dqn dueling network
+
+    Args:
+        input_width: Network input width
+        num_actions: Number of actions
+        lstm_width: Number of LSTM layer units
+        relu_width: Number of RELU layer units
+        combiner: Ways of combining the value and advantage layers
+
+    Returns: tensorflow keras Model
+
+    """
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
     lstm_layer = tf.keras.layers.LSTM(lstm_width)(input_layer)
     relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(lstm_layer)
@@ -61,7 +122,7 @@ def create_lstm_dueling_dqn_network(input_width: int, num_actions: int,
     advantage = tf.keras.layers.Dense(num_actions, activation='linear')(relu_layer)
     if combiner == 'avg':
         dueling_q_layer = value + (advantage - tf.reduce_mean(advantage, axis=1, keepdims=True))
-    elif combiner == 'mean':
+    elif combiner == 'max':
         dueling_q_layer = value + (advantage - tf.reduce_max(advantage, axis=1, keepdims=True))
     else:
         raise Exception(f'Unknown combiner function ({combiner})')
@@ -72,6 +133,19 @@ def create_lstm_dueling_dqn_network(input_width: int, num_actions: int,
 @gin.configurable
 def create_lstm_categorical_dqn_network(input_width: int, num_actions: int,
                                         lstm_width: int = 32, relu_width: int = 32, num_atoms: int = 51):
+    """
+    Creates a lstm categorical dqn network
+
+    Args:
+        input_width: Network input width
+        num_actions: Number of actions
+        lstm_width: Number of LSTM layer units
+        relu_width: Number of RELU layer units
+        num_atoms: Num of atoms in the output
+
+    Returns: tensorflow keras Model
+
+    """
     input_layer = tf.keras.layers.Input(shape=(None, input_width))
     lstm_layer = tf.keras.layers.LSTM(lstm_width)(input_layer)
     relu_layer = tf.keras.layers.Dense(relu_width, activation='relu')(lstm_layer)
