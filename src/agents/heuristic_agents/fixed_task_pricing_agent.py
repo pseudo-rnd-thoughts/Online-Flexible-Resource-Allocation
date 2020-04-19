9f4280630438508a3b76322f5a201e3b83b890b9
StringTheory <mark.m.towers@gmail.com>
2020-04-19 01:49:39 +0100

    Reverted agent back to reinforcement learning agent to use for the heuristic policy training

--- src/agents/heuristic_agents/fixed_task_pricing_agent.py
+++ src/agents/heuristic_agents/fixed_task_pricing_agent.py
@@ -7,20 +7,22 @@ The bid is then random for the task
 from __future__ import annotations
 
 import random as rnd
-from typing import List
+from typing import List, Optional
 
-from agents.task_pricing_agent import TaskPricingAgent
+import numpy as np
+
+from agents.rl_agents.rl_agents import TaskPricingRLAgent
 from env.server import Server
 from env.task import Task
 
 
-class FixedTaskPricingAgent(TaskPricingAgent):
+class FixedTaskPricingAgent(TaskPricingRLAgent):
     """
     Fixed task pricing agents
     """
 
     def __init__(self, agent_num: int, bid_tasks_number: int, max_price: int = 5, **kwargs):
-        TaskPricingAgent.__init__(self, f'Fixed Task Pricing Agent {agent_num}', **kwargs)
+        TaskPricingRLAgent.__init__(self, f'Fixed Task Pricing Agent {agent_num}', **kwargs)
 
         self.bid_tasks_number = bid_tasks_number
         self.max_price = max_price
@@ -30,3 +32,13 @@ class FixedTaskPricingAgent(TaskPricingAgent):
             return float(rnd.randint(1, self.max_price))
         else:
             return 0
+
+    @staticmethod
+    def _network_obs(task: Task, allocated_tasks: List[Task], server: Server, time_step: int) -> np.ndarray:
+        pass
+
+    def _train(self, states, actions, next_states, rewards, dones) -> float:
+        return 0
+
+    def _save(self, custom_location: Optional[str] = None):
+        pass
