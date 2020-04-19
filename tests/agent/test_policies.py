305bcfeae25d5a9d88862e0227f88788ac6c56e1
StringTheory <mark.m.towers@gmail.com>
2020-04-19 10:24:08 +0100

    Fixed policy testing with the new epsilon greedy mechanism

--- tests/agent/test_policies.py
+++ tests/agent/test_policies.py
@@ -5,7 +5,6 @@ Tests the linear exploration method used in the DQN agent
 # TODO add comments
 from agents.rl_agents.agents.dqn import TaskPricingDqnAgent, ResourceWeightingDqnAgent
 from agents.rl_agents.neural_networks.dqn_networks import create_lstm_dqn_network
-from agents.rl_agents.rl_policies import EpsilonGreedyTaskPricingPolicy, EpsilonGreedyResourceAllocationPolicy
 from env.environment import OnlineFlexibleResourceAllocationEnv
 
 
@@ -14,30 +13,30 @@ def test_epsilon_policy():
     env = OnlineFlexibleResourceAllocationEnv('agent/settings/basic.env')
     state = env.reset()
 
-    pricing_dqn_agent = TaskPricingDqnAgent(0, create_lstm_dqn_network(9, 5))
-    weighting_dqn_agent = ResourceWeightingDqnAgent(0, create_lstm_dqn_network(16, 5))
-
     num_servers = len(state.server_tasks.keys())
-    pricing_policy = EpsilonGreedyTaskPricingPolicy(
-        pricing_dqn_agent, initial_epsilon=1, final_epsilon=0.1, update_frequency=1,
-        epsilon_steps=(len(env._unallocated_tasks) + (1 if state.auction_task is not None else 0)) * num_servers)
-    weighting_policy = EpsilonGreedyResourceAllocationPolicy(
-        weighting_dqn_agent, initial_epsilon=1, final_epsilon=0.1, update_frequency=1,
-        epsilon_steps=env._total_time_steps * num_servers)
+    pricing_dqn_agent = TaskPricingDqnAgent(0, create_lstm_dqn_network(9, 5), initial_epsilon=1, final_epsilon=0.1,
+                                            update_frequency=1,
+                                            epsilon_steps=(len(env._unallocated_tasks) + (
+                                                1 if state.auction_task is not None else 0)) * num_servers)
+    weighting_dqn_agent = ResourceWeightingDqnAgent(0, create_lstm_dqn_network(16, 5), initial_epsilon=1,
+                                                    final_epsilon=0.1, update_frequency=1,
+                                                    epsilon_steps=env._total_time_steps * num_servers)
 
     done = False
     while not done:
         if state.auction_task is not None:
             actions = {
-                server: pricing_policy.bid(state.auction_task, tasks, server, state.time_step)
+                server: pricing_dqn_agent.bid(state.auction_task, tasks, server, state.time_step, training=True)
                 for server, tasks in state.server_tasks.items()
             }
         else:
             actions = {
-                server: weighting_policy.weight(tasks, server, state.time_step)
+                server: weighting_dqn_agent.weight(tasks, server, state.time_step, training=True)
                 for server, tasks in state.server_tasks.items()
             }
         state, rewards, done, _ = env.step(actions)
 
-    assert pricing_policy.total_actions == pricing_policy.epsilon_steps
-    assert round(pricing_policy.epsilon, 2) == 0.1
+    assert pricing_dqn_agent.total_actions == pricing_dqn_agent.epsilon_steps
+    assert round(pricing_dqn_agent.epsilon, 2) == 0.1
+
+    assert 0 < weighting_dqn_agent.total_actions
