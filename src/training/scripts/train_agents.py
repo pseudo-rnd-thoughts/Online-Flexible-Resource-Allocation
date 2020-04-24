f53ff95a678dfa129137e2141b8d1289cc0c7af8
StringTheory <mark.m.towers@gmail.com>
2020-04-24 01:23:55 +0100

    Add summary histograms for auction prices

--- src/training/scripts/train_agents.py
+++ src/training/scripts/train_agents.py
@@ -32,8 +32,10 @@ class EvalResults:
 
     def __init__(self):
         # Auction attributes
-        self.winning_prices: float = 0
+        self.total_winning_prices: float = 0
+        self.winning_prices: List[float] = []
         self.num_auctions: int = 0
+        self.auction_actions: List[List[float]] = []
 
         # Resource allocation attributes
         self.num_completed_tasks: int = 0
@@ -50,7 +52,9 @@ class EvalResults:
             rewards: Dictionary of rewards
         """
         for server, price in rewards.items():
-            self.winning_prices += price
+            self.total_winning_prices += price
+            self.winning_prices.append(price)
+        self.auction_actions.append([action for action in actions.values()])
         self.num_auctions += 1
 
     def resource_allocation(self, actions, rewards):
@@ -80,10 +84,13 @@ class EvalResults:
         Args:
             episode: Episode number
         """
-        tf.summary.scalar('Eval winning prices', self.winning_prices, episode)
+        tf.summary.scalar('Eval total winning prices', self.total_winning_prices, episode)
+        tf.summary.scalar('Eval total prices', self.total_prices, episode)
+        tf.summary.histogram('Eval auction actions', tf.convert_to_tensor(self.auction_actions), episode)
+        tf.summary.histogram('Eval winning auction prices', tf.convert_to_tensor(self.winning_prices), episode)
+
         tf.summary.scalar('Eval number of completed tasks', self.num_completed_tasks, episode)
         tf.summary.scalar('Eval number of failed tasks', self.num_failed_tasks, episode)
-        tf.summary.scalar('Eval total prices', self.total_prices, episode)
         tf.summary.scalar('Eval percent tasks', (self.num_completed_tasks + self.num_failed_tasks) / self.num_auctions,
                           episode)
         tf.summary.scalar('Eval completed failed task ratio', self.num_completed_tasks / (self.num_failed_tasks + 1),
